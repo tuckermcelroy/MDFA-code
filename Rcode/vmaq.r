5d38e82e893bd4f254c0f8cefe485f59a70b2037
Tucker McElroy <33321700+tuckermcelroy@users.noreply.github.com>
2023-07-25 15:39:17 -0400

    Update vmaq.r
    main code

--- Rcode/vmaq.r
+++ Rcode/vmaq.r
@@ -9,15 +9,23 @@ vmaq.sim <- function(theta.array,innovar.matrix,T.sim)
   #   theta.array: n x n x q dimensional array of coefficients
   #   innovar.matrix: n x n innovation covariance matrix
   
+  q <- dim(theta.array)[3]
+  n <- dim(theta.array)[1]
+  theta.matrix <- matrix(theta.array,nrow=n)
   
-  
-  eps.old <- rnorm(2)
-x.sim <- NULL
-for(t in 1:T)
-{
-  eps.next <- rnorm(2)
-  x.next <- eps.next + theta.matrix %*% eps.old
-  eps.old <- eps.next
-  x.sim <- cbind(x.sim,x.next)
+  eps.init <- NULL
+  for(j in 1:q)
+  {
+    eps.init <- rbind(eps.init,t(chol(innovar.matrix)) %*% rnorm(n))
+  }
+  eps.next <- eps.init
+  x.sim <- NULL
+  for(t in 1:T.sim)
+  {
+    x.next <- rnorm(n) + theta.matrix %*% eps.next
+    eps.next <- rbind(t(chol(innovar.matrix)) %*% rnorm(n),eps.next[1:(n*q-n)])
+    x.sim <- cbind(x.sim,x.next)
+  }
+  x.sim <- ts(t(x.sim))
+  return(x.sim)
 }
-x.sim <- ts(t(x.sim))
