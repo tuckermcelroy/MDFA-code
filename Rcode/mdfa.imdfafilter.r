7a7c0fcff1333f3ac18534dc77ab9fe06a37fc5f
Tucker McElroy <33321700+tuckermcelroy@users.noreply.github.com>
2024-11-27 12:05:49 -0500

    Update mdfa.imdfafilter.r
    completed first draft

--- Rcode/mdfa.imdfafilter.r
+++ Rcode/mdfa.imdfafilter.r
@@ -8,7 +8,7 @@ mdfa.imdfafilter <- function(frf,spec,lambda,eta,mu,q)
   #	computes optimal concurrent moving average filter
   #		as approx of given target filter, using I-ATS MDFA method,
   #		based upon the moving average filter class of length q.
-  #   This code is applied multivariately
+  #   This code is applied multivariately, one component at a time
   #	inputs:
   #		frf is array N x N x Grid of complex entries, the target
   #			frequency response function Psi(e^{-i lambda})
@@ -28,40 +28,56 @@ mdfa.imdfafilter <- function(frf,spec,lambda,eta,mu,q)
   N <- dim(spec)[1]
   grid <- dim(frf)[3]
   m <- floor(grid/2)
+  lambda.ft <- exp(-1i*2*pi*grid^{-1}*(seq(1,grid) - (m+1)))	## this is e^{-i lambda}
 
-  W.fcn <- (1 + pmax(0,abs(2*pi*grid^{-1}*(seq(1,grid) - (m+1))) - mu))^eta
-     
-  fpsi <- NULL
-	fmat <- NULL
-	lambda.ft <- exp(-1i*2*pi*grid^{-1}*(seq(1,grid) - (m+1)))	## this is e^{-i lambda}
-
-	opt.val <- do.call(cbind,lapply(seq(1,grid),function(i) W.fcn[i] * 
-	                                  frf[,,i] %*% spec[,,i] %*% Conj(t(frf[,,i]))))
-	opt.val <- grid^{-1}*opt.val %*% (rep(1,grid) %x% diag(N))
-	for(k in 0:(q-1))
-	{
-		fpsi.new <- do.call(cbind,lapply(seq(1,grid),function(i) W.fcn[i] * frf[,,i] %*% spec[,,i]))
- 		fpsi.new <- grid^{-1}*fpsi.new %*% (lambda.ft^{-k} %x% diag(N))
-		fpsi <- cbind(fpsi,fpsi.new)
-		fmat.new <- grid^{-1}*matrix(spec,nrow=N) %*% (lambda.ft^{-k} %x% diag(N))
-		if(k==0) { 
-			fmat <- fmat.new 
-			fzero <- fmat.new
-		} else {
-			if(k==1) {
-				fmat <- cbind(fmat,fmat.new)
-				fmat <- rbind(fmat,cbind(t(fmat.new),fzero))
-			} else {
-				side.mat <- fmat[1:(dim(fmat)[2]-N),(dim(fmat)[2]+1-N):dim(fmat)[2],drop=FALSE]
-				fmat <- cbind(fmat,rbind(fmat.new,side.mat))
-				fmat <- rbind(fmat,cbind(t(fmat.new),t(side.mat),fzero))
-			}
-		}
-	} 
-  fpsi <- Re(fpsi)
-  fmat <- Re(fmat)
-  opt <- solve(fmat,fpsi)
-  opt.val <- opt.val - t(fpsi) %*% opt
+  opts <- NULL
+  opt.vals <- NULL
+  for(n in 1:N)
+  {
+    W.fcn <- (1 + pmax(0,abs(2*pi*grid^{-1}*(seq(1,grid) - (m+1))) - mu))^eta[n]
+    opt.val <- do.call(cbind,lapply(seq(1,grid),
+                            function(i) frf[n,,i,drop=FALSE] %*% spec[,,i] %*% 
+                                        Conj(t(frf[n,,i,drop=FALSE]))))
+	  opt.val <- grid^{-1}*opt.val %*% (W.fcn %x% diag(N))
+		fpsi <- NULL
+	  fmat <- NULL
+	  for(k in 1:q)
+	  {
+		  fpsi.new <- do.call(cbind,lapply(seq(1,grid),
+		                        function(i) frf[n,,i,drop=FALSE] %*% spec[,,i]))
+ 		  fpsi.new <- grid^{-1}*fpsi.new %*% ((W.fcn*lambda.ft^{1-k}) %x% diag(N))
+		  fpsi <- cbind(fpsi,fpsi.new)
+		  fmat.col <- NULL
+		  for(j in 1:q)
+		  {
+		    fmat.new <- grid^{-1}*matrix(spec,nrow=N) %*% ((W.fcn * lambda.ft^{j-k}) %x% diag(N))
+		    spec.temp <- do.call(cbind,lapply(seq(1,grid),
+		                          function(i) t(spec[,,i]) %*% t(frf[n,,i,drop=FALSE]) %*% 
+		                                      frf[n,,i,drop=FALSE] %*% spec[,,i] ))
+		    fmat.temp <- grid^{-1}*spec.temp %*% ((W.fcn * lambda.ft^{-j-k}) %x% diag(N))
+		    fmat.new <- fmat.new - lambda[n]*fmat.temp
+		    spec.temp <- do.call(cbind,lapply(seq(1,grid),
+		                                      function(i) spec[,,i] %*% Conj(t(frf[n,,i,drop=FALSE])) %*% 
+		                                        Conj(frf[n,,i,drop=FALSE]) %*% t(spec[,,i]) ))
+		    fmat.temp <- grid^{-1}*spec.temp %*% ((W.fcn * lambda.ft^{j+k}) %x% diag(N))
+		    fmat.new <- fmat.new - lambda[n]*fmat.temp
+		    spec.temp <- do.call(cbind,lapply(seq(1,grid),
+		                                      function(i) spec[,,i] %*% Conj(t(frf[n,,i,drop=FALSE])) %*% 
+		                                        frf[n,,i,drop=FALSE] %*% spec[,,i] ))
+		    fmat.temp <- grid^{-1}*spec.temp %*% ((W.fcn * lambda.ft^{j-k}) %x% diag(N))
+		    fmat.new <- fmat.new + 2*lambda[n]*fmat.temp
+		    fmat.col <- rbind(fmat.col,fmat.new)
+		  }
+		  fmat <- cbind(fmat,fmat.col)
+	  }  
+		fpsi <- Re(fpsi)
+    fmat <- Re(fmat)
+    opt <- solve(fmat,fpsi)
+    opt.val <- opt.val - t(fpsi) %*% opt
+  
+    opts <- rbind(opts,opt)
+    opt.vals <- c(opt.vals,opt.val)  
+  }    
     
   return(list(opts,opt.vals))
 }
